5ab2b92c7eb285343dc458fdb6be4cdbb7551c55
Demiurg39 <chiryagov2014@gmail.com>
2024-05-01 00:06:27 +0600

    added executeQuery/Update methods

--- app/src/main/java/org/database/JDBC.java
+++ app/src/main/java/org/database/JDBC.java
@@ -3,23 +3,25 @@ package org.database;
 import java.sql.*;
 
 public class JDBC {
-  public static void main(String[] args) throws SQLException, ClassNotFoundException {
-    // Load JDBC driver !!! FOR LINUX !!!
-    Class.forName("org.mariadb.jdbc.Driver");
+  private Connection connection;
+
+  public JDBC(String user, String password) throws SQLException {
+    connection = DriverManager.getConnection("jdbc:mysql://localhost/megalinedb", user, password);
   }
 
-  // Define the connect method outside of the main method
-  static Statement setup_db(String user, String password) {
-    try {
-      // Connect to the database
-      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/megalinedb", user, password);
-      // Create a Statement object for executing SQL queries
-      Statement stmt = conn.createStatement();
-      return stmt;
-    } catch (SQLException e) {
-      // Exception handling if there is an error in connection or statement creation
-      System.err.println("Error connecting to the database or creating Statement: " + e.getMessage());
-      return null;
+  public ResultSet executeQuery(String sql) throws SQLException {
+    Statement statement = connection.createStatement();
+    return statement.executeQuery(sql);
+  }
+
+  public int executeUpdate(String sql) throws SQLException {
+    Statement statement = connection.createStatement();
+    return statement.executeUpdate(sql);
+  }
+
+  public void close() throws SQLException {
+    if (connection != null) {
+      connection.close();
     }
   }
 }
