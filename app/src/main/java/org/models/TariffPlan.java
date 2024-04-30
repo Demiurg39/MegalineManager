dc43d7ae9db04fb486cf96d2ffb07fcb28ab581b
Demiurg39 <chiryagov2014@gmail.com>
2024-05-01 05:05:50 +0600

    fixed constructor

--- app/src/main/java/org/models/TariffPlan.java
+++ app/src/main/java/org/models/TariffPlan.java
@@ -1,20 +1,35 @@
 package org.models;
 
 public class TariffPlan {
+  private static int nextId = 1;
+  private int id;
   private double price;
   private double internetSpeed;
   private String description;
 
-
-    public TariffPlan(double price, double internetSpeed, String description) throws IllegalArgumentException {
-        if (internetSpeed > 200) {
-            throw new IllegalArgumentException("Unreachable internet speed");
-        }
-        this.price = price;
-        this.internetSpeed = internetSpeed;
-        this.description = description;
+  public TariffPlan(double price, double internetSpeed, String description) throws IllegalArgumentException {
+    if (internetSpeed > 200) {
+      throw new IllegalArgumentException("Unreachable internet speed");
     }
+    this.id = nextId++;
+    this.price = price;
+    this.internetSpeed = internetSpeed;
+    this.description = description;
+  }
 
+  public TariffPlan(double price, double internetSpeed) throws IllegalArgumentException {
+    if (internetSpeed > 200) {
+      throw new IllegalArgumentException("Unreachable internet speed");
+    }
+    this.id = nextId++;
+    this.price = price;
+    this.internetSpeed = internetSpeed;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+  
   public double getPrice() {
     return this.price;
   }
