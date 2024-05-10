6412d990328d16c21b9bc6e1d1fe373d7a186d83
Demiurg39 <chiryagov2014@gmail.com>
2024-05-11 01:46:50 +0600

    Added employe classes

---
+++ app/src/main/java/org/database/manager/models/Employe.java
@@ -0,0 +1,11 @@
+package org.database.manager.models;
+
+import java.util.Hashtable;
+
+public class Employe {
+  
+  private int id;
+  private Hashtable password;
+  private String priveleges;
+
+}
