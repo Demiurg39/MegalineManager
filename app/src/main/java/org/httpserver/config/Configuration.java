865459661be945ef911ce9cf4d19e8c0f4844334
Demiurg39 <chiryagov2014@gmail.com>
2024-05-11 01:48:04 +0600

    added configuration classes

---
+++ app/src/main/java/org/httpserver/config/Configuration.java
@@ -0,0 +1,15 @@
+package org.httpserver.config;
+
+public class Configuration {
+
+  private int port;
+
+  public int getPort() {
+    return port;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+}
