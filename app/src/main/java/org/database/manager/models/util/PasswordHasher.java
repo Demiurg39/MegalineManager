46e4d0a438200943d36391a676b62939c78b9ac7
Demiurg39 <chiryagov2014@gmail.com>
2024-05-12 01:08:52 +0600

    added passwordCheck method

--- app/src/main/java/org/database/manager/models/util/PasswordHasher.java
+++ app/src/main/java/org/database/manager/models/util/PasswordHasher.java
@@ -7,6 +7,8 @@ public class PasswordHasher {
   public static String getHash(String password) {
     return BCrypt.hashpw(password, BCrypt.gensalt());
   }
+
+  public static boolean passwordCheck(String hash, String password) {
+    return hash.equals(getHash(password)) ? true : false;
+  }
 }
-
-
