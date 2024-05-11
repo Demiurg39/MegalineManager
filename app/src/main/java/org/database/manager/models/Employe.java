c66452f96b98899adb68e85aa2baefd00fa2a89f
Demiurg39 <chiryagov2014@gmail.com>
2024-05-12 01:04:02 +0600

    writed employe model

--- app/src/main/java/org/database/manager/models/Employe.java
+++ app/src/main/java/org/database/manager/models/Employe.java
@@ -1,11 +1,41 @@
 package org.database.manager.models;
 
-import java.util.Hashtable;
+import org.database.manager.models.util.PasswordHasher;
+import org.database.manager.models.Privileges;
 
 public class Employe {
-  
-  private int id;
-  private Hashtable password;
-  private String priveleges;
+
+  private int employeId;
+  private String passwordHash;
+  private Privileges privileges;
+
+  public Employe(int employeId, String password) {
+    this.employeId = employeId;
+    this.passwordHash = PasswordHasher.getHash(password);
+    this.privileges = Privileges.USER;
+  }
+
+  public Employe(int id, String password, Privileges privileges) {
+    this(id, password);
+    this.privileges = privileges;
+  }
+
+  public int getEmployeId() {
+    return employeId;
+  }
+
+  public String getPasswordHash() {
+    return passwordHash;
+  }
+
+  public Privileges getPrivileges() {
+    return privileges;
+  }
+
+  public void setPrivileges(Privileges privileges, Privileges who) {
+    if (who == Privileges.ADMIN)
+      this.privileges = privileges;
+    return;
+  }
 
 }
