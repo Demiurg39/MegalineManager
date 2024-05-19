1d19ebe51006a55d662c7fcdb4539fc15c5775d2
Demiurg39 <chiryagov2014@gmail.com>
2024-05-19 20:16:30 +0600

    Added Server connected 200 status code

--- app/src/main/java/org/megaline/httpserver/http/HttpStatusCode.java
+++ app/src/main/java/org/megaline/httpserver/http/HttpStatusCode.java
@@ -10,7 +10,12 @@ public enum HttpStatusCode {
   /* --- SERVER_ERROR --- */
   SERVER_ERROR_500_INTERNAL_ERROR(500, "Internal Error"),
   SERVER_ERROR_501_NOT_IMPLEMENTED(501, "Not Iplemented"),
-  SERVER_ERROR_505_HTTP_VERSION_NOT_SUPPORTED(505, "Http Version Not Supported");
+  SERVER_ERROR_505_HTTP_VERSION_NOT_SUPPORTED(505, "Http Version Not Supported"),
+
+  /* --- SERVER_CODES --- */
+  SERVER_CONNECTED_200(200, HttpVersion.HTTP_1_1 + " 200 OK" ),
+
+  ;
 
   public final int STATUS_CODE;
   public final String MESSAGE;
