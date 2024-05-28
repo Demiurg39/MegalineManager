e28d9601531c8108ec9c5c4b454d8529a563e0b3
demiurg39 <chiryagov2014@gmail.com>
2024-05-29 00:22:31 +0600

    renamed

--- app/src/main/java/org/megaline/core/util/DHCP.java
+++ app/src/main/java/org/megaline/core/util/DHCP.java
@@ -1,29 +1,19 @@
 package org.megaline.core.util;
 
-public class dhcp {
-    import java.util.Random;
+import java.util.Random;
 
-    public class IPAddressGenerator {
+public class DHCP {
 
-        private static final Random RANDOM = new Random();
+    private static final Random RANDOM = new Random();
 
-        // Генерация случайного IP-адреса в формате "xxx.xxx.xxx.xxx"
-        public static String generateIPAddress() {
-            StringBuilder ipAddress = new StringBuilder();
-            for (int i = 0; i < 4; i++) {
-                ipAddress.append(RANDOM.nextInt(256)); // Генерируем случайное число от 0 до 255
-                if (i < 3) {
-                    ipAddress.append(".");
-                }
+    public static String generateIPAddress() {
+        StringBuilder ipAddress = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            ipAddress.append(RANDOM.nextInt(256));
+            if (i < 3) {
+                ipAddress.append(".");
             }
-            return ipAddress.toString();
         }
-
-        // Пример использования
-        public static void main(String[] args) {
-            String ipAddress = generateIPAddress();
-            System.out.println("Random IP Address: " + ipAddress);
-        }
-    }
-
+        return ipAddress.toString();
+   }
 }
